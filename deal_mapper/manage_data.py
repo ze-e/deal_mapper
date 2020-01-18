45bca0a8c268a739fc26a00288255fddd87e7250
ze-e <white.noise616@gmail.com>
2020-01-17 20:17:41 -0800

    temp removed some features

--- deal_mapper/manage_data.py
+++ deal_mapper/manage_data.py
@@ -29,7 +29,7 @@ class Manage_Data:
 #        'signnn':'https://signnn.com/listings/',
         'sambazis':'https://www.sambazisretailgroup.com/listings/',
         'themansourgroup':'http://www.themansourgroup.com/query_listingsNEW2.cfm?region=all&viewType=0&division=&sortBy=0&ltr=&priceMin=&priceMax=&CapRateMin=&states=&search=&CapRateMax=',
-        'nnndeal':'http://nnndeal.com/nnn-properties-for-sale/',
+#        'nnndeal':'http://nnndeal.com/nnn-properties-for-sale/',
         'snydercarlton':'http://snydercarlton.com/get_listings.cfm',
         'issenbergbritti':'http://www.issenbergbritti.com/all-listings/',
         'issenbergbritti2':'http://www.issenbergbritti.com/all-listings/page/2/',
